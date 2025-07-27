34d591143943d0b1f5d936fba7fb31551cf54a1c
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-07-27 23:33:59 +0530

    add array

---
+++ array/reverse.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+  vector<int> reverseArray( vector<int> &arr) {
+
+    int start = 0;
+    int end = arr.size() - 1;
+    while (start <= end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    return arr;
+  }
