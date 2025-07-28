cc29dceffd6fc65cc4040e2308934800fe1400c7
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-07-28 22:47:46 +0530

    add array

---
+++ array/shift.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  vector<int> shiftRight(vector<int> &arr) {
+    int temp = arr[arr.size() - 1];
+    int start = 0;
+    int last = arr.size() - 1;
+    while (start < last) {
+      arr[last] = arr[last - 1];
+      --last;
+    }
+
+    arr[0] = temp;
+
+    return arr;
+  }
+};
