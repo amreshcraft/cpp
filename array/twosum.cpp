8d5ecdb4a4b0ac3455b074933df7d5dcf458c937
Amresh <210040598+amreshcraft@users.noreply.github.com>
2025-08-06 23:10:07 +0530

    Create twosum.cpp

---
+++ array/twosum.cpp
@@ -0,0 +1,24 @@
+ vector<int> twoSum(vector<int> &nums, int target) {
+
+
+    unordered_map<int, int> hash;
+
+
+    for (int i = 0; i < nums.size(); i++) {
+
+      int complement = target - nums[i];
+
+      if (hash.find(complement) != hash.end()) {
+
+        return {hash[complement], i};
+
+      }
+
+
+      hash[nums[i]] = i;
+
+    }
+
+    return {-1, -1};
+
+  }
