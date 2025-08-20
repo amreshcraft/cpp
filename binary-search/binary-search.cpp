2df573f37ef6190f182b7f7793e57e329cfcbee2
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-08-20 23:15:30 +0530

    add binary-search

---
+++ binary-search/binary-search.cpp
@@ -0,0 +1,18 @@
+int binarySearch(const vector<int> &arr, int target) {
+
+  int start = 0;
+  int end = arr.size() - 1;
+
+  while (start <= end) {
+
+    int mid = (start + end) / 2;
+    if (arr[mid] == target)
+      return mid;
+    if (arr[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1; // placeholder
+}
