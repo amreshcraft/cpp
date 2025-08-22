069b771313930a8859815f0870301e71074e6ee1
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-08-22 23:03:57 +0530

    binary-search: first and last occurence

---
+++ binary-search/first-and-last.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+  vector<int> searchRange(vector<int> &nums, int target) {
+
+    vector<int> res(2, -1);
+
+    // Find the first occurrence
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        res[0] = mid;
+        right = mid - 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    // Find the last occurrence
+    left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        res[1] = mid;
+        left = mid + 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return res;
+  }
+};
